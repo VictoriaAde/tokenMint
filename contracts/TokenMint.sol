bea66432d62e34220af0d1f1a9d5084e34f91134
Victoria <adedayovicky123@gmail.com>
2024-02-25 17:15:41 +0100

    CHORE

--- contracts/TokenMint.sol
+++ contracts/TokenMint.sol
@@ -5,10 +5,9 @@ contract AdeToken {
     string public name = "AdeToken";
     string public symbol = "ADK";
     uint256 public totalSupply;
-    uint8 public decimals =  18;
+    uint8 public decimals = 0;
 
     address public owner;
-
 
     mapping(address => uint256) public balanceOf;
     mapping(address => mapping(address => uint256)) public allowance;
@@ -17,12 +16,13 @@ contract AdeToken {
     event Approval(address indexed owner, address indexed spender, uint256 value);
 
     constructor(uint256 initialSupply) {
-        totalSupply = initialSupply *  10**decimals;
+        totalSupply = initialSupply * 10**decimals;
         balanceOf[msg.sender] = totalSupply;
+        owner = msg.sender;
     }
 
     modifier onlyOwner() {
-        require(msg.sender == owner, "You are not owner");
+        require(msg.sender == owner, "You are not the owner");
         _;
     }
 
@@ -34,7 +34,7 @@ contract AdeToken {
     }
 
     function transfer(address _to, uint256 _value) public returns (bool success) {
-        require(balanceOf[msg.sender] >= _value);
+        require(balanceOf[msg.sender] >= _value, "Insufficient balance");
         balanceOf[msg.sender] -= _value;
         balanceOf[_to] += _value;
         emit Transfer(msg.sender, _to, _value);
@@ -48,8 +48,8 @@ contract AdeToken {
     }
 
     function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
-        require(_value <= balanceOf[_from]);
-        require(_value <= allowance[_from][msg.sender]);
+        require(_value <= balanceOf[_from], "Insufficient balance");
+        require(_value <= allowance[_from][msg.sender], "Allowance exceeded");
         balanceOf[_from] -= _value;
         balanceOf[_to] += _value;
         allowance[_from][msg.sender] -= _value;
@@ -58,10 +58,9 @@ contract AdeToken {
     }
 
     function burn(uint256 _value) public returns (bool success) {
-        require(balanceOf[msg.sender] >= _value);
+        require(balanceOf[msg.sender] >= _value, "Insufficient balance");
         balanceOf[msg.sender] -= _value;
         totalSupply -= _value;
         return true;
     }
-
 }
